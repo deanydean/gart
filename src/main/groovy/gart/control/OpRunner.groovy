74f9d840c7ac6e6ee2a6291e3d57618645b398a0
Deany Dean <deanydean@shells>
2017-07-28 20:36:24 +0100

    Updated copyright

--- src/main/groovy/gart/control/OpRunner.groovy
+++ src/main/groovy/gart/control/OpRunner.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Matt Dean
+ * Copyright 2017 Matt Dean
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
